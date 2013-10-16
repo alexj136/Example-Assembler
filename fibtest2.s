e5508a8289159a73d289a56294ee9330250ce476
Alex Jeffery <apj21@sussex.ac.uk>
2013-10-16 09:08:14 +0100

    :-)

--- fibtest2.s
+++ fibtest2.s
@@ -1,37 +1,16 @@
 .text
 printf_str: .asciz "%d"
 error_str: .asciz "Error: reached end of function without return"
-
 .globl main
 main:
-
-movl %esp, %ebp
-
-# BEGIN RETURN STATEMENT 0
-# BEGIN CALL 0 TO 'fibonacci'
-pushl %ebp
-movl $2, %eax    # CALL FIBONACCI WITH 5
+movl $6, %eax
 pushl %eax
 addl $4, %esp
 movl %esp, %ebp
 call fibonacci
-movl %ebp, %esp
-popl %ebp
-# END CALL 0 TO 'fibonacci'
-
-# pushl %eax
-# pushl $printf_str
-# call printf
-
-ret
-# END RETURN STATEMENT 0
-
-# END OF FUNCTION ERROR CODE |
-pushl $error_str           # |
-call printf                # |
-movl $0, %ebx              # |
-movl $1, %eax              # |
-int $0x80                  # |
+movl %eax, %ebx
+movl $1, %eax
+int $0x80
 
 .globl fibonacci
 fibonacci:
@@ -50,10 +29,10 @@ movl %ecx, %eax
 # END BOOLEAN EXPRESSION 0
 cmpl $0, %eax
 je else_branch_0
-# BEGIN RETURN STATEMENT 1
+# BEGIN RETURN STATEMENT 0
 movl $0, %eax    # INTEGER LITERAL
 ret
-# END RETURN STATEMENT 1
+# END RETURN STATEMENT 0
 jmp if_end_0
 else_branch_0:
 if_end_0:
@@ -72,15 +51,15 @@ movl %ecx, %eax
 # END BOOLEAN EXPRESSION 1
 cmpl $0, %eax
 je else_branch_1
-# BEGIN RETURN STATEMENT 2
+# BEGIN RETURN STATEMENT 1
 movl $1, %eax    # INTEGER LITERAL
 ret
-# END RETURN STATEMENT 2
+# END RETURN STATEMENT 1
 jmp if_end_1
 else_branch_1:
-# BEGIN RETURN STATEMENT 3
+# BEGIN RETURN STATEMENT 2
 # BEGIN ARITHMETIC EXPRESSION 0
-# BEGIN CALL 2 TO 'fibonacci'
+# BEGIN CALL 1 TO 'fibonacci'
 pushl %ebp
 # BEGIN ARITHMETIC EXPRESSION 2
 movl $2, %eax    # INTEGER LITERAL
@@ -95,9 +74,9 @@ movl %esp, %ebp
 call fibonacci
 movl %ebp, %esp
 popl %ebp
-# END CALL 2 TO 'fibonacci'
+# END CALL 1 TO 'fibonacci'
 pushl %eax
-# BEGIN CALL 1 TO 'fibonacci'
+# BEGIN CALL 0 TO 'fibonacci'
 pushl %ebp
 # BEGIN ARITHMETIC EXPRESSION 1
 movl $1, %eax    # INTEGER LITERAL
@@ -112,12 +91,12 @@ movl %esp, %ebp
 call fibonacci
 movl %ebp, %esp
 popl %ebp
-# END CALL 1 TO 'fibonacci'
+# END CALL 0 TO 'fibonacci'
 popl %ebx
 addl %ebx, %eax
 # END ARITHMETIC EXPRESSION 0
 ret
-# END RETURN STATEMENT 3
+# END RETURN STATEMENT 2
 if_end_1:
 # END IF STATEMENT 1
 
@@ -127,3 +106,4 @@ call printf                # |
 movl $0, %ebx              # |
 movl $1, %eax              # |
 int $0x80                  # |
+
